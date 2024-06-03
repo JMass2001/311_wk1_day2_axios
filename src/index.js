55ca0e15222ff12520d633d847b515eb8e70cc53
JMass2001 <156865380+JMass2001@users.noreply.github.com>
2024-06-02 22:12:44 -0500

    homework

--- src/index.js
+++ src/index.js
@@ -1,30 +1,52 @@
-// import axios here
+const axios = require("axios");
+const API_KEY = "d771b19ef336ed8381def3a60b574464";
+const API_BASE_URL = "https://api.themoviedb.org/3";
 
-// Please note that it is normally not considered best practice to commit 
-// api keys to github as it presents a security risk. It is done here only 
-// for practice purposes as we are sharing the same account
-const api_key = 'd771b19ef336ed8381def3a60b574464'
+const discoverMovie = async () => {
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/discover/movie?api_key=${API_KEY}`
+    );
 
-const discoverMovie = () => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
-  // code here
-}
+    if (response.data && response.data.results) {
+      return {
+        data: response.data,
+        status: response.status,
+        headers: response.headers,
+      };
+    } else {
+      throw new Error("No results property found in the response data");
+    }
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    throw error;
+  }
+};
 
-const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
-}
+const getMovieById = async (id) => {
+  const response = await axios.get(
+    `${API_BASE_URL}/movie/${id}?api_key=${API_KEY}`
+  );
+  return response.data;
+};
 
-const getMovieByIdFailure = () => {
-  const fakeId = 1 // FAKE ID HERE
-  const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
-  // code here
-}
-
-
+const getMovieByIdFailure = async () => {
+  const fakeId = 0;
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/movie/${fakeId}?api_key=${API_KEY}`
+    );
+    return response.status;
+  } catch (error) {
+    if (error.response) {
+      return error.response.status;
+    }
+    throw error;
+  }
+};
 
 module.exports = {
   discoverMovie,
   getMovieById,
-  getMovieByIdFailure
-}
+  getMovieByIdFailure,
+};
